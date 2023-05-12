e574175e1c9c61c833016ed12fdaec59fa01fef1
jjiny0716 <jjiny3773@naver.com>
2023-05-12 11:50:47 +0900

    feat: header 구현

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "@/components/Header/Header.styles";
+
+const Header = () => {
+  return <HeaderContainer>
+    <p className="title">문제 이름</p>
+  </HeaderContainer>;
+};
+
+export default Header;
